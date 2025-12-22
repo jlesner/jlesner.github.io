74b1a0b365023844744408a419d9c8be4599907d
jlesner <jlesner@ucsb.edu>
2025-12-22 14:03:19 -0800

    added host-agnostic static build

--- commands.bash
+++ commands.bash
@@ -10,7 +10,9 @@ bundle install
 # jekyll serve -l -H localhost -P 4000
 bundle exec jekyll serve -l -H localhost -P 4000
 
+# host-agnostic static build
+bundle exec jekyll build --config _config.yml,_config_static.yml
+
 
 # apt install libimage-exiftool-perl
 # exiftool -Title="" -Author="" -Producer="" "`wslpath -u 'C:\Users\chris\OneDrive\jlesner\resume\jlesner_cv_2025_0010.pdf'`"
-
